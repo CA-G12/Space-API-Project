268205708cedeadca12e761fcb876d90148ef833
shamskhodary <shams.khodary@gmail.com>
2022-08-02 16:55:41 +0300

    Added the js folder which has the fetch file where we added xhr request, in addition to the dom and testing file Relates #1

---
+++ JS/fetch.js
@@ -0,0 +1,21 @@
+let url1 = "https://api.spacexdata.com/v5/launches";
+let url2 = "https://api.nasa.gov/techport/api/projects/17792?api_key=DEMO_KEY";
+
+const fetch = (url, cb) => {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let data = JSON.parse(xhr.responseText);
+      cb(data);
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send();
+};
+
+// fetch(url1, (data) => {
+//   console.log(data);
+//   fetch(url2, (data) => {
+//     console.log(data);
+//   });
+// });
